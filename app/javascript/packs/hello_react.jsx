5f5ffb6c2c5bafe150931b3397b2c3b9e2145811
Mutuba <danielmutubait@gmail.com>
2023-04-10 10:59:47 +0300

    configure webpacker, typescript and render hello react

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,11 +5,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = (props) => <div>Hello {props.name}!</div>;
+// const Hello = (props) => <div>Hello {props.name}!</div>;
+export default (props) => <div>Hello {props.name}!</div>;
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="Mutuba" />,
+    <Hello name="React" />,
     document.body.appendChild(document.createElement("div"))
   );
 });
